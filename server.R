2e905a72aba45ae18b255c90f8ac6ece6deb55cf
Jose Espinosa <kadomu@gmail.com>
2017-04-13 08:33:17 +0200

    First working version of plot

--- server.R
+++ server.R
@@ -6,18 +6,60 @@
 #
 
 library(shiny)
+library(ggplot2)
+
+source ("/Users/jespinosa/git/phecomp/lib/R/plotParamPublication.R")
+
+## Reading data
+csv_file <- "https://raw.githubusercontent.com/cbcrg/bengen/master/CACHE/cache.csv"
+n_fields <- max(count.fields(csv_file, sep = ','))
+bench_tbl <- read.table(csv_file, header = FALSE, sep = ",", col.names = paste0("V",seq_len(n_fields)), fill = TRUE)
+bench_tbl$V1 <-  gsub ("bengen/", "", bench_tbl$V1, ignore.case = TRUE)
+bench_tbl$V2 <-  gsub ("bengen/", "", bench_tbl$V2, ignore.case = TRUE)
+
+## aes
+## colorblind friendly palette
+cb_palette <- c("#999999", "#E69F00", "#56B4E9",
+                "#009E73", "#F0E442", "#0072B2", 
+                "#D55E00", "#CC79A7", "#000000", 
+                "#00009B")
+
+## Avoid problems if too many groups
+cb_palette <- rep (cb_palette, 10)
+
+# ggplot(bench_tbl, aes(V1, y=V5, fill=V2) ) +
+#     geom_bar(position="dodge", stat="identity")
 
 shinyServer(function(input, output) {
-
-  output$distPlot <- renderPlot({
-
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-  })
+#     output$bedGraphRange_tab <- renderUI({
+#         sliderInput("bedGraphRange", label = h4("Data range:"), 
+#                     min = 0, max = 1000, 
+#                     value = c(10, 100), 
+#                     step= 1)
+#     })
+    output$groups_tab <- renderUI({
+        checkboxGroupInput( "groups", label = h4("Groups to render:"), 
+                            choices = unique(bench_tbl$V3), 
+                            selected = unique(bench_tbl$V3))
+        
+    })
+    # Combine the selected variables into a new data frame
+    selected_data <- reactive({
+        if(!is.null(input$groups)) {
+            bench_tbl <- subset(bench_tbl, V3 %in% input$groups)
+        }        
+    })
+    output$bench_results <- renderPlot({
+        if(!is.null(input$groups)) {
+            ggplot(selected_data(), aes(V3, y=V5, fill=V2) ) +
+                   scale_fill_manual(values=cb_palette) +
+                   geom_bar(position="dodge", stat="identity")
+        }
+    })
+#   output$tbl_res <- renderTable({
+    #     as.data.frame(dfFileEnv_range())
+    #     as.data.frame(dfFilePhases_range())
+#     as.data.frame(dfFilePhases_range()$ranges_p)    
+#   })
 
 })
--- ui.R
+++ ui.R
@@ -7,24 +7,39 @@
 
 library(shiny)
 
-shinyUI(fluidPage(
-
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 50,
-                  value = 30)
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
-  )
-))
+shinyUI(
+    fluidPage(
+        titlePanel("bengen"),        
+#         headerPanel("Hello Shiny!")
+               
+        sidebarPanel(
+            conditionalPanel(condition="input.tabs_p=='Plot'",
+#                              uiOutput("bedGraphRange_tab"),                    
+                             uiOutput("groups_tab")
+            ),
+            conditionalPanel(condition="input.tabs_p=='About'",
+                             h4("Introduction") 
+            )
+        ),
+        
+        mainPanel(
+            tabsetPanel(
+                tabPanel("Plot",
+                         fluidRow(                  
+                             column(12,
+                                    plotOutput("bench_results", height=800),
+                                    plotOutput("legend_track", height=50),
+                                    plotOutput("envInfo", height=20))#,
+                             #                            textOutput("text1")
+                         )#,
+                         #column(3, downloadButton("all_plot_tiff", "Download snapshot"))#,
+                ),            
+                tabPanel("About",
+                         HTML('<p>bengen</p>')),
+                
+                id="tabs_p"
+                
+                )
+            )
+        )
+)
